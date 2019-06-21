fa63ccca01586180c37580da15dff30bb3de5d35
Viktor Koop <viktor.koop@googlemail.com>
2019-06-21 09:38:23 +0200

    move to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.3.40"
+
+    id("application")
+
+    kotlin("jvm") version kotlinVersion
+}
+
+group = "de.vkoop"
+version = "1.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    compile("com.github.librepdf:openpdf:1.2.17")
+    compile("info.picocli:picocli:3.9.5")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "de.vkoop.pdfutils.AppKt"
+}
