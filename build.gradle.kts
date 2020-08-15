5dba1a17bb8200bd3ef595d3b65e508449c89a98
Viktor Koop <viktor.koop@googlemail.com>
2020-08-16 00:50:52 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.3.72"
+    val kotlinVersion = "1.4.0"
 
     id("application")
 
     kotlin("jvm") version kotlinVersion
 
-    id("com.github.ben-manes.versions") version "0.28.0"
+    id("com.github.ben-manes.versions") version "0.29.0"
 }
 
 group = "de.vkoop"
-version = "1.0.1"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
@@ -22,8 +22,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("com.github.librepdf:openpdf:1.3.19")
-    implementation("info.picocli:picocli:4.3.2")
+    implementation("com.github.librepdf:openpdf:1.3.20")
+    implementation("info.picocli:picocli:4.5.0")
 }
 
 tasks.withType<KotlinCompile> {
